4b5fc3b026195e2be7e2bc15080a4f3ab7e7a000
Dion Kennedy <20101554@mail.wit.ie>
2025-10-07 14:03:16 +0100

    Full review page.

--- movies/src/main.jsx
+++ movies/src/main.jsx
@@ -4,6 +4,8 @@ import { BrowserRouter, Route, Navigate, Routes, Link } from "react-router";
 import HomePage from "./pages/homePage";
 import MoviePage from "./pages/movieDetailsPage";
 import FavoriteMoviesPage from "./pages/favoriteMoviesPage";
+import MovieReviewPage from "./pages/movieReviewPage";
+
 
 const App = () => {
   return (
@@ -21,6 +23,8 @@ const App = () => {
         <Route path="/movies/:id" element={<MoviePage />} />
         <Route path="/" element={<HomePage />} />
         <Route path="*" element={ <Navigate to="/" /> } />
+        <Route path="/reviews/:id" element={ <MovieReviewPage /> } />
+
       </Routes>
     </BrowserRouter>
   );
