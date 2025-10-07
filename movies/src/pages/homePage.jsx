764038fe6ab777d02e60c1f0158e298aa24102f1
Dion Kennedy <20101554@mail.wit.ie>
2025-10-07 13:45:34 +0100

    Add to favourites logic completed; Dummy favourites page.

--- movies/src/pages/homePage.jsx
+++ movies/src/pages/homePage.jsx
@@ -24,6 +24,12 @@ const genreId = Number(genreFilter);
     else setGenreFilter(value);
   };
 
+  const addToFavorites = (movieId) => {
+    const updatedMovies = movies.map((m) =>
+      m.id === movieId ? { ...m, favorite: true } : m
+    );
+    setMovies(updatedMovies);
+  };
 
 
   useEffect(() => {
@@ -54,7 +60,7 @@ const genreId = Number(genreFilter);
               />
 
         </Grid>
-        <MovieList movies={displayedMovies}></MovieList>
+        <MovieList movies={displayedMovies} selectFavorite={addToFavorites} />
       </Grid>
     </Grid>
   );
