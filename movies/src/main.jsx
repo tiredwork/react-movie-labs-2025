3bd9fe88e4c009d542f4746ce3d248043c1da8de
Dion Kennedy <20101554@mail.wit.ie>
2025-10-16 21:07:31 +0100

    Added Upcoming movies page.

--- movies/src/main.jsx
+++ movies/src/main.jsx
@@ -10,6 +10,7 @@ import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import MoviesContextProvider from "./contexts/moviesContext";
 import AddMovieReviewPage from './pages/addMovieReviewPage'
+import UpcomingMoviesPage from './pages/upcomingMoviesPage'
 
 
 
@@ -36,6 +37,7 @@ const App = () => {
             <Route path="/" element={<HomePage />} />
             <Route path="*" element={ <Navigate to="/" /> } />
             <Route path="/reviews/form" element={ <AddMovieReviewPage /> } />
+            <Route path="/movies/upcoming" element={ <UpcomingMoviesPage /> } />
 
           </Routes>
         </MoviesContextProvider>
